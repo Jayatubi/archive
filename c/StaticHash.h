417c8c99c18f335751913fa0273f0591752ee0ad
Jayatubi <yzk0370@gmail.com>
2014-10-02 23:44:19 +0800

    Update StaticHash.h

--- c/StaticHash.h
+++ c/StaticHash.h
@@ -3,7 +3,9 @@ struct Hash
     template <unsigned int N, unsigned int I>
     struct Helper
     {
-        __forceinline static unsigned int Calculate(const char (&str)[N])
+        // __forceinline could make the string turn into integer in the optimize phase on MSVC. Thus, it couldn't be used as const value
+        /*__forceinline*/
+        constexpr static unsigned int Calculate(const char (&str)[N])
         {
             return (Helper<N, I - 1>::Calculate(str) ^ str[I - 1]) * PRIME;
         }
@@ -12,14 +14,15 @@ struct Hash
     template <unsigned int N>
     struct Helper<N, 1>
     {
-        __forceinline static unsigned int Calculate(const char (&str)[N])
+        /*__forceinline*/
+        constexpr static unsigned int Calculate(const char (&str)[N])
         {
             return (OFFSET ^ str[0]) * PRIME;
         }
     };
  
     template <unsigned int N>
-    static unsigned int Calculate(const char (&str)[N])
+    constexpr static unsigned int Calculate(const char (&str)[N])
     {
         return Helper<N, N>::Calculate(str);
     }
